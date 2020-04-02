e2f89c9cd1564b32c5039fe9dfab5ef9416655f9
will brown <will.brown039@gmail.com>
2020-04-02 18:00:00 -0400

    Fix array uniforms for Relaxed Vulkan
    Changed it so that the relaxed vulkan does its own checks instead
    of relying on the existing ones

--- glslang/MachineIndependent/ParseHelper.cpp
+++ glslang/MachineIndependent/ParseHelper.cpp
@@ -6560,6 +6560,30 @@ TIntermNode* TParseContext::declareVariable(const TSourceLoc& loc, TString& iden
             
             growGlobalUniformBlock(loc, type, identifier, nullptr);
             symbol = symbolTable.find(identifier);
+
+            if (!symbol) {
+                error(loc, "error adding uniform to global uniform block", identifier.c_str(), ""); 
+            }
+
+            layoutObjectCheck(loc, *symbol); // xxTODO: good idea to do this for the member? do we want to do type-qualifier checks before creation instead?
+
+            // xxTODO: should do a final layoutObjectCheck() for the global buffer ?
+            {
+                auto blockQualifier = globalUniformBlock->getType().getQualifier();
+                auto typeList = *globalUniformBlock->getWritableType().getWritableStruct();
+                bool memberWithLocation = false; // no locations allowed;
+                bool memberWithoutLocation = true;
+
+                // xxTODO: most of these checks won't do anything, since we're uniforms...
+                //        not sure if we need them to catch disallowed qualifiers (e.g. xfb_..) will test
+                //fixBlockLocations(loc, blockQualifier, typeList, memberWithLocation, memberWithoutLocation); // won't matter, no locations
+                //fixXfbOffsets(currentBlockQualifier, typeList);   // won't matter, no xfb
+                fixBlockUniformOffsets(currentBlockQualifier, typeList); // xxTODO: the glslangToSpv module appears to generate offsets on its own, so we don't need to explicitly set it for the members; still probably a good thing to do?
+                
+                layoutObjectCheck(loc, *globalUniformBlock);
+            }
+
+            return nullptr;
         }
     }
 
